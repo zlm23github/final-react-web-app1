35945846ca67d9b96e2a1b906d7468ef985b3f9f
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-21 18:23:18 -0400

    1

--- src/pet/service/auth-service.js
+++ src/pet/service/auth-service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const SERVER_API_URL = "http://localhost:8080/api";
 // const SERVER_API_URL = process.env.REACT_APP_API_BASE;
-const SERVER_API_URL = "https://fianl-project.onrender.com/api"
+const SERVER_API_URL = "https://fianl-project.onrender.com/api";
 
 const USERS_URL = `${SERVER_API_URL}/users`;
 
